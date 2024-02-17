df106f6fd3fb22c3977638cf8736335d78741102
yerinra <yerinaleph@gmail.com>
2024-02-17 23:26:19 +0900

    🎉 Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
