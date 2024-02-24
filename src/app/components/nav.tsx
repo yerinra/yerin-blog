e24e4460af7557665b0ea543dd60c12f373a75d0
yerinra <yerinaleph@gmail.com>
2024-02-24 21:59:12 +0900

    💄 Design: navBar 디자인

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,12 +2,24 @@
 import { links } from "@/lib/data";
 import { useActiveSectionStore } from "@/lib/hooks/useActivePath";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { buttonVariants } from "@/components/ui/button";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { LinkName } from "@/lib/types";
 
 export default function Nav() {
+  const pathname = usePathname();
   const { activeLink, setActiveLink } = useActiveSectionStore();
+
+  useEffect(() => {
+    let newActiveLink;
+    if (pathname.includes("posts")) newActiveLink = "Blog";
+    if (pathname === "/") newActiveLink = "Home";
+    if (pathname === "/portfolio") newActiveLink = "Portfolio";
+    if (pathname === "/about") newActiveLink = "About";
+    setActiveLink(newActiveLink as LinkName);
+  }, [pathname, setActiveLink]);
 
   return (
     <nav>
